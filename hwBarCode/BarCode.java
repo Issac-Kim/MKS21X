284098b1d90bfac7b111cc75f19e2b648d15bc87
Issac-Kim <ikim1@stuy.edu>
2015-12-22 22:54:59 -0500

    finished and checked methods other than compare and equals

--- hwBarCode/BarCode.java
+++ hwBarCode/BarCode.java
@@ -4,11 +4,13 @@ public class BarCode implements Comparable{
     private static final String[] code = {"||:::", ":::||", "::|:|", "::||:", ":|::|", ":|:|:", ":||::", "|:::|", "|::|:", "|:|::", "||:::"};
     public BarCode(String zip){
 	try{
-	    if(zip.length() != 5){
+	    if(zip.length() == 5){
 		_zip = zip;
 		_checkDigit = checkSum();
 	    }
-	    throw new RuntimeException();
+	    else{
+		throw new RuntimeException();
+	    }
 	}
 	catch(NumberFormatException e){
 	    throw new RuntimeException();
@@ -19,8 +21,29 @@ public class BarCode implements Comparable{
 	_checkDigit = x._checkDigit;
     }
     private int checkSum(){
-	int zip = Integer.parseInt(_zip);
 	int ans = 0;
-	
+	for(int i = 0; i < _zip.length(); i++){
+	    ans += Integer.parseInt(_zip.substring(i, i + 1));
+	}
+	while(ans >= 10){
+	    ans = ans % 10;
+	}
+	return ans;
+    }
+    public String toString(){
+	int ans =  Integer.parseInt(_zip);
+	return "|" + BarCode.code[Integer.parseInt(_zip.substring(0, 1))]+  BarCode.code[Integer.parseInt(_zip.substring(1, 2))] +  BarCode.code[Integer.parseInt(_zip.substring(2, 3))] + BarCode.code[Integer.parseInt(_zip.substring(3, 4))] + BarCode.code[Integer.parseInt(_zip.substring(4, 5))] + BarCode.code[_checkDigit] + "|";
+    }
+    public boolean equals(Object other){
+	if(other instanceof BarCode){
+	    return toString().equals(other.toString());
+	}
+	return false;
+    }
+    public int compareTo(Object other){
+	if(equals(other)){
+	    return 0;
+	}
+	return 1;
     }
 }
