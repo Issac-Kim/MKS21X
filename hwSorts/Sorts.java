13f148e71304de0531f2ed28844aef2efd22bfae
Issac-Kim <ikim1@stuy.edu>
2016-01-04 20:52:49 -0500

    uploaded previous insertion sort and wrote selection sort

--- hwSorts/Sorts.java
+++ hwSorts/Sorts.java
@@ -1,3 +1,29 @@
 public class Sorts{
-    static void insertion(int[] arr)
+    public static void Selection(int[] arr){
+	for(int i = 0; i < arr.length; i++){
+	    int index = i;
+	    int smallest = arr[i];
+	    for(int l = i; l < arr.length; l++){
+		if(arr[l] < smallest){
+		    smallest = arr[l];
+		    index = l;
+		}
+	    }
+	    int temp = arr[index];
+	    arr[index] = arr[i];
+	    arr[i] = temp;
+	}
+    }
+    public static void Insertion(int[] arr){
+	for(int i = 0; i < arr.length; i++){
+	    int k = i;
+	    while(k > 0 && arr[k] < arr[k - 1]){
+		int temp = arr[k];
+		arr[k] = arr[k - 1];
+		arr[k - 1] = temp;
+		k--;
+	    }
+	}
+    }
+    
 }
